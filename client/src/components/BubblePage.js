82a05f93dad0ba1ed5393894b960966439deefb3
Julian Leon <jleon1992@gmail.com>
2020-07-24 12:08:17 -0400

    added an add color function with post request

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,9 +3,16 @@ import {axiosWithAuth} from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import ColorForm from './ColorForm'
+
+const initialColor = {
+  color: "",
+  code: { hex: "" }
+};
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const [newColor, setNewColor] = useState(initialColor)
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
   useEffect(() => {
@@ -16,9 +23,20 @@ const BubblePage = () => {
       setColorList(res.data)
     })
   })
+
+ 
+  const addColor = e => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post('http://localhost:5000/api/colors', newColor)
+    .then(res => {
+      console.log(res)
+    })
+  }
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorForm newColor={newColor} addNewColor={addColor} setNewColor={setNewColor} />
+      <ColorList colors={colorList} updateColors={setColorList}  />
       <Bubbles colors={colorList} />
     </>
   );
